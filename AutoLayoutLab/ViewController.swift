96f200bcfc1ccf394d092e562f533b2630f7d287
janezhu1618 <janezhu@pursuit.org>
2018-11-14 17:54:54 -0500

    first commit - finished 6 questions

---
+++ AutoLayoutLab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Jane Zhu on 11/14/18.
+//  Copyright © 2018 Jane Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
